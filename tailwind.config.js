7e7a14ff08a1f0044cd86305f589be12deeb318a
Quashnock <awq@uw.edu>
2024-03-28 22:21:02 -0700

    Commit Message

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        lora: "'Lora' , serif",
+        inter: "'Inter', serif",
+        Merienda: "'Merienda', serif"
+
+      }
+    },
   },
   plugins: [],
 }
